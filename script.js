a0b0eb9a3547b7cd10fd70d644ab390c64749865
Md Sarfraj <116196482+mdsarfarj@users.noreply.github.com>
2023-11-20 23:10:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+  const checkboxes = document.querySelectorAll('.toggle');
+
+        checkboxes.forEach(checkbox => {
+            checkbox.addEventListener('change', function() {
+                let checkedCount = document.querySelectorAll('.toggle:checked').length;
+                if (checkedCount > 2) {
+                    this.checked = false;
+                }
+            });
+        });
